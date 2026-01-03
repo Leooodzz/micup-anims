26da6520b625e7d6148ccebe75f7989b45bfcbe4
Leooodzz <garciariverajorgeleonel2011@gmail.com>
2026-01-02 20:27:37 -0600

    Add files via upload

---
+++ VPOSE_KEYFRAME.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        Time = 0,
+        ["RightShoulder"] = CFrame.Angles(math.rad(-60), 0, math.rad(35)),
+        ["LeftShoulder"]  = CFrame.Angles(math.rad(-60), 0, math.rad(-35)),
+
+        ["RightElbow"] = CFrame.Angles(math.rad(-10), 0, 0),
+        ["LeftElbow"]  = CFrame.Angles(math.rad(-10), 0, 0),
+    }
+}
